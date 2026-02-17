ae5c7c49134cc927ba12a47127155c36aecb2b8b
Guilherme Faramiglio <guilherme.busato@outlook.com>
2026-02-17 17:35:51 -0300

    Nível intermediário - loop aninhado
    Nível intermediário - loop aninhado;
    movimentação do cavalo, em L.

--- xadrez.c
+++ xadrez.c
@@ -29,11 +29,18 @@ int main() {
         rainha++;
     } while (rainha <= 8);
     printf("\n");
-    
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    // Movimentação do Cavalo (estruturas for e while - loop aninhado)
+    // Duas casas para baixo e uma para a esquerda
+    printf("Movimentação do Cavalo:\n");
+    int i = 1;
+    while (i <= 2) {
+        for (int j = 1; j <= 1; j++) {
+            printf("Baixo: Casa %d, Esquerda: Casa %d\n", i, j);
+        }
+        i++;
+    }
+    printf("\n");
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
